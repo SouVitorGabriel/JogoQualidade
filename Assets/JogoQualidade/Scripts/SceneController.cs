832e9092a9af86a7247baa56d1868944f75c3d92
Biel <vitor.s.alencar@hotmail.com>
2019-11-04 05:04:41 -0300

    Adicionados assets e criadas cenas iniciais

---
+++ Assets/JogoQualidade/Scripts/SceneController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField]
+    private Image imgFade;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = imgFade.GetComponent<Animator>();
+    }
+    public void MudaCena(string sceneName)
+    {
+        StartCoroutine(Fading(sceneName));
+    }
+
+    IEnumerator Fading(string sceneName)
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(()=>imgFade.color.a==1);
+        SceneManager.LoadScene(sceneName);
+    }
+}
